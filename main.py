8a402562983bb504f388029ec6326425cb38f89d
codeWithAdi234 <121111744+codeWithAdi234@users.noreply.github.com>
2022-12-26 12:56:03 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,173 @@
+questions1 = [
+  "Q1)What does not grow on tree according to a popular Hindi saying",
+  "Q2)Which city is known as Pink City in India",
+  "Q3)Who among the following was killed during 'Operation Bluestar' of 1984",
+  "Q4)Which god is also known as ‘Gauri Nandan’",
+  "Q5)Current Railway Minister of India is ",
+  "Q6)Which was the first mountain peak above 8,000 metres in height to be summited by humans?",
+  "Q7)Milinda-Panha is a dialogue between King Menander or Milinda and which Buddhist monk?",
+  "Q8)Which poet in the court of Mughal Ruler Bahadur Shah Zafar wrote the ‘Dastan-e-Ghadar’, a personal account of the 1857 revolt?",
+  "Q9)Who, in 1978, became the first person to be born in the continent of Antarctica? ",
+  "Q10)Which former Indian President died as a result of a road accident"
+]
+
+Option1 = [
+  "A.Money  B.Flowers  C.Leaves  D.Fruits",
+  "A.Banglore  B.Maysore  C.Jaipur  D.Kochi",
+  "A.Baba Santa Singh  B. Haji Mastan  C.Jarnail Singh Bhindrawale  D.Homi Jehangir Bhabha",
+  "A.Agni  B.Indra  C.Hanuman  D.Ganesha",
+  "A.Mamta Banarjee  B.Ram Vilash  C. Ashwini Vaishnaw  D.Piyush Goyal",
+  "A. Annapurna  B. Lhotse  C. Kanchenjunga  D. Makalu",
+  "A. Asanga  B. Nagasena  C. Mahadharmarakshita  D. Dharmaraksita",
+  "A.Mir Taqi Mir  B.Mohammad Ibrahim Zauq  C.Zahir Dehlvi  D.Abul- Qasim Ferdowsi",
+  "A.Emilio Palma  B.James Weddell  C.Charles Wilkes  D.Junko Tabei",
+  "A.Rajendra Prasad  B.Faqruddin Ali Ahmed  C.Giani Zail Singh  D.R.Venkatraman"
+]
+
+Ans1 = ["A", "C", "C", "D", "A", "A", "B", "C", "A", "C"]
+
+
+print(questions1[0])
+print(Option1[0])
+
+user = input("\nEnter the Correct Option: ")
+
+if (user == Ans1[0]):
+    print(
+      "\nCongrats!! Correct Ans You Won Dhanrashi of Rs10,000\n")
+
+else:
+    print("\nThe Ans is Worng better luck next time!! Correct   Ans is: \n", Ans1[0])
+
+
+print(questions1[1])
+print(Option1[1])
+
+user = input("Enter the Correct Option: ")
+
+if (user == Ans1[1]):
+    print("Congrats!! Correct Ans You Won Dhanrashi of Rs50,000")
+
+else:
+    print(
+      "The Ans is Worng better luck next time!! Correct Ans is:", Ans1[1])
+
+
+
+print(questions1[2])
+print(Option1[2])
+
+user = input("\nEnter the Correct Option: ")
+
+if (user == Ans1[2]):
+    print(
+      "\nCongrats!! Correct Ans You Won Dhanrashi of Rs1,00,000\n")
+
+else:
+  print("\nThe Ans is Worng better luck next time!! Correct   Ans is: \n", Ans1[2])
+
+
+
+
+
+print(questions1[3])
+print(Option1[3])
+
+user = input("\nEnter the Correct Option: ")
+
+if (user == Ans1[3]):
+    print(
+      "\nCongrats!! Correct Ans You Won Dhanrashi of Rs1,50,000\n")
+
+else:
+  print("\nThe Ans is Worng better luck next time!! Correct   Ans is: \n", Ans1[3])
+
+
+
+
+
+print(questions1[4])
+print(Option1[4])
+
+user = input("\nEnter the Correct Option: ")
+
+if (user == Ans1[4]):
+    print(
+      "\nCongrats!! Correct Ans You Won Dhanrashi of Rs3Lakh\n")
+
+else:
+  print("\nThe Ans is Worng better luck next time!! Correct   Ans is: \n", Ans1[4])
+
+
+
+
+
+print(questions1[5])
+print(Option1[5])
+
+user = input("\nEnter the Correct Option: ")
+
+if (user == Ans1[5]):
+    print(
+      "\nCongrats!! Correct Ans You Won Dhanrashi of Rs5Lakh\n")
+
+else:
+  print("\nThe Ans is Worng better luck next time!! Correct   Ans is: \n", Ans1[5])
+
+
+
+print(questions1[6])
+print(Option1[6])
+
+user = input("\nEnter the Correct Option: ")
+
+if (user == Ans1[6]):
+    print(
+      "\nCongrats!! Correct Ans You Won Dhanrashi of Rs7Lakh\n")
+
+else:
+  print("\nThe Ans is Worng better luck next time!! Correct   Ans is: \n", Ans1[6])
+
+
+
+print(questions1[7])
+print(Option1[7])
+
+user = input("Enter the Correct Option: ")
+
+if (user == Ans1[7]):
+    print("Congrats!! Correct Ans You Won Dhanrashi of Rs10lakh")
+
+else:
+    print(
+      "The Ans is Worng better luck next time!! Correct Ans is:", Ans1[7])
+
+
+
+print(questions1[8])
+print(Option1[8])
+
+user = input("\nEnter the Correct Option: ")
+
+if (user == Ans1[8]):
+    print(
+      "\nCongrats!! Correct Ans You Won Dhanrashi of Rs1CR\n")
+
+else:
+  print("\nThe Ans is Worng better luck next time!! Correct   Ans is: \n", Ans1[8])
+
+
+
+
+
+print(questions1[9])
+print(Option1[9])
+
+user = input("\nEnter the Correct Option: ")
+
+if (user == Ans1[9]):
+    print(
+      "\nCongrats!! Correct Ans You Won Dhanrashi of Rs7CR\n")
+
+else:
+  print("\nThe Ans is Worng better luck next time!! Correct   Ans is: \n", Ans1[9])
